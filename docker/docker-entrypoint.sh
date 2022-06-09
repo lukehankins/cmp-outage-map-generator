8403702266d5fc56d6b2babd1653d0bf5eb58d44
Luke Hankins <luke@ethersmith.com>
2022-06-08 21:57:36 -0400

    Update docker-entrypoint.sh to handle errors

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,8 +6,12 @@ if [ -z "${RUN_SERVER}" ]; then
     echo XXXXXXXXXXXXXXXXX
     ls -l /tmp
     echo XXXXXXXXXXXXXXXXX
-    python3 ./upload-to-s3.py --verbose --debug --file "/tmp/$FILENAME" --destination "$FILENAME"
+    if [ -f "/tmp/$FILENAME" ]; then
+        python3 ./upload-to-s3.py --verbose --debug --file "/tmp/$FILENAME" --destination "$FILENAME"
+    else
+        echo "ERROR: /tmp/$FILENAME not found.  Exiting." 
+    fi
 else
-    echo "run server here"
+    echo "running server"
     gunicorn --bind :8080 --workers 2 website:app
 fi
